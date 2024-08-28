0e265faebd930ee2c87ac2b164f58e34f27df4c8
Shunneao <jose842006@gmail.com>
2024-08-28 11:40:26 -0600

    v1:hola

---
+++ resources/components/app.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <h1 class="text-center mt-5">Hola desde Vue.js</h1>
+        <p class="text-center">Este es un componente para Vue</p>
+
+        <div class="text-center mt-4">
+            <p>Mensaje: {{ message }}</p>
+            <button @click="changeMessage" class="btn btn-primary">Cambiar mensaje</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hola, people!'
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'Bueno, un diseño base normal';
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
+.text-center {
+    text-align: center;
+}
+.mt-5 {
+    margin-top: 3rem;
+}
+.mt-4 {
+    margin-top: 1.5rem;
+}
+</style>
